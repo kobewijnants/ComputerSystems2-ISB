1f14d7d4f918cc9741312968a623a5abc23f6c9c
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-07 17:05:19 +0100

    Add PowerShell script to manipulate services and files

--- Powershell/W15P3.ps1
+++ Powershell/W15P3.ps1
@@ -4,28 +4,28 @@
 # @since 01/02/2024        #
 ############################
 
-#
+# Get all processes.
 Get-Process
 
-#
+# Open notepad.
 notepad
 
-#
+# Get all processes of notepad.
 Get-Process notepad
 
-#
+# Get all processes of notepad and format the output in a list.
 Get-Process notepad | Format-List
 
-#
+# Get all processes of notepad and format the output in a table.
 Get-Process notepad | Format-Table
 
-#
+# Get all processes of notepad and format the output in a table with the "Name" and "Path" properties.
 Get-Process notepad | Format-Table Name,Path
 
-#
+# Get all processes of notepad and format the output in a table with all properties.
 Get-Process notepad | Format-Table *
 
-#
+# Stop all processes of notepad.
 Get-Process notepad | Stop-Process
 
 # Get the first process with the name notepad.
@@ -40,16 +40,16 @@ Group-Object -Property notepad
 # Count the number of notepad processes.
 Measure-Object -Property notepad
 
-#
+# Get the first 5 services.
 Get-Service | Select-Object -First 5
 
-#
+# Get date and format the output Date, Month, Year.
 Get-date | Select-Object -Property Day,Month,Year
 
-#
+# Get all processes and format the output in a table with the "Name" and "LastWriteTime" and put in a file.
 Get-ChildItem | Select-Object LastWriteTime | Sort-Object Name, LastWriteTime > C:\Users\elias\Desktop\output.txt
 
-#
+# Get all services and group them by their status.
 Get-Service | Group-Object -Property Status
 
 # Get the number of processes + Format the output.
@@ -59,19 +59,21 @@ Get-Process | Measure-Object | Select-Object -Property Count
 Get-Service | Group-Object -Property Status
 
 ############################
-# Wat is the difference between the following commands?
+
+# Question: What is the difference between the following commands?
 Get-date | Select-Object -Property day, month, year
 Get-date | Format-Table -Property day, month, year
 # Answer: The first command creates a new object, while the second command formats the output.
+
 ############################
 
 # Get the date and store it in a variable.
 $datum = Get-Date
 
-#
+# Format the output (DayOfWeek)
 $datum.DayOfWeek
 
-#
+# Format the output (Month)
 $datum.Month
 
 # Add a day to the date.
@@ -80,31 +82,31 @@ $datum.AddDays(1)
 # () is nessesary to execute the comandlet first to get the object and then the method can be called on the object.
 (Get-Date).DayOfWeek
 
-#
+# Get all processes and store them in a variable.
 $processes = Get-Process
 
-#
+# Show process 9.
 $processes[9]
 
-#
+# Show the name of process 9.
 $processes[9].ProcessName
 
-#
+# Show all process names.
 $processes.ProcessName
 
-# 
+# Show all members of the process object.
 "" | Get-Member
 
-#
+# Get string 2 to 5.
 "Hallo Wereld".Substring(2,5)
 
-#
+# Define an array.
 $rij = 1,2,3,4,5
 
-#
+# Show the first element of the array.
 Get-Member -InputObject $rij
 
-#
+# Show the length of the array.
 $rij.Length
 ############################
 # -eq, -ne, -lt, -le, -gt, -ge, -like, -not, !, -and, -or
@@ -131,30 +133,34 @@ Get-Process notepad | % {$_.kill()}
 
 # Get all processes and calculate the sum of their ids.
 Get-Process | % {$a += $_.id } ; $a
+
 ############################
-# 1. Toon een lijst met alle properties van alle subdirectories in je homedirectory waarvan de naam met een D begint.
+
+# 1. Show a list of all properties of all subdirectories in your home directory whose names start with a D.
 Get-ChildItem D* -Directory | Format-List *
 
-# 2. Toon een tabel met enkel Fullname en Attributes van alle files en directories in je homedirectory, omgekeerd gesorteerd op naam.
+# 2. Display a table with only Fullname and Attributes of all files and directories in your home directory, sorted in reverse by name.
 Get-ChildItem | Sort-Object Fullname, Attributes -Descending | Format-Table Fullname, Attributes
 
-# 3. Bekijk de methods en properties van Get-Process. Toon de titel van je Powershell venster. Doe dit nogmaals, maar nu in uppercase.
+# 3. View the methods and properties of Get-Process. Show the title of your Powershell window. Do this again, but now in uppercase.
 (Get-Process notepad).MainWindowTitle
 
-# 4. Toon de services waar de DHCP service afhankelijk van is.
+# 4. Show the services that the DHCP service depends on.
 (Get-Service dhcp).ServicesDependedOn
 
-# 5. Tel de VirtualMemorySize op van alle processen die met een c beginnen.
+# 5. Add the VirtualMemorySize of all processes that start with c.
 Get-Service c* | ForEach-Object {$size += $_.VirtualMemorySize}
 $size
 
-# 6. Maak een bestand "filelist.txt" in je homedirectory met daarin de volledige padnamen van "c:\windows\win.ini" en "c:\windows\system.ini".
+# 6. Create a file "filelist.txt" in your home directory containing the full path names of "c:\windows\win.ini" and "c:\windows\system.ini".
 New-Item -Path C:\Users\elias\ -Name filelist.txt -ItemType File
 Add-Content -Path C:\Users\elias\filelist.txt -Value "C:\Windows\win.ini"
 Add-Content -Path C:\Users\elias\filelist.txt -Value "C:\Windows\system.ini"
 
-# Toon de inhoud op het scherm van alle files in "filelist.txt". Dit moet ook nog werken als je een file toevoegt aan "filelist.txt".
+# Display the on-screen contents of all files in "filelist.txt". This should also work if you add a file to "filelist.txt".
 Get-Content -Path C:\Users\elias\filelist.txt | ForEach-Object {Get-Content $_}
 
 # Drop the "filelist.txt" file.
 Remove-Item -Path C:\Users\elias\filelist.txt
+
+############################
--- Powershell/W16P3.ps1
+++ Powershell/W16P3.ps1
@@ -3,3 +3,110 @@
 # @see https://eliasdh.com #
 # @since 01/02/2024        #
 ############################
+
+# Get the first 10 services and convert the output to csv.
+Get-Service | Select-Object -First 10 | Export-Csv service.csv
+
+# Open the file.
+Invoke-Item service.csv
+
+# Import the file.
+$service = import-CSV service.csv
+
+# Show the fifth service.
+$service[4]
+
+# Delete the file.
+Remove-Item service.csv
+
+# Get the first 10 services and convert the output to json.
+Get-Service | Select-Object -First 10 | ConvertTo-Json # Or xml or csv
+
+# Convert the json to a powershell object.
+'{"name":"Elias De Hondt","leeftijd":22}' | ConvertFrom-Json
+
+# Get the first 5 services and convert the output to json and save it to a file.
+Get-Service | Select-Object -first 5 -Property Name, Status | ConvertTo-Json | Out-File service.json
+
+# Get the content of the file and convert it to a powershell object.
+$service = Get-Content .\service.json | ConvertFrom-Json
+
+# Delete the file.
+Remove-Item service.json
+
+# Gat all services and convert the output to html and save it to a file.
+Get-Service | ConvertTo-Html | Out-File service.html
+
+# Delete the file.
+Remove-Item service.html
+
+############################
+
+New-Item -Path C:\Users\elias\Desktop -Name files.csv -ItemType File
+Add-Content -Path C:\Users\elias\Desktop\files.csv -Value "Path;Type"
+Add-Content -Path C:\Users\elias\Desktop\files.csv -Value "C:\Temp\Studenten;Directory"
+Add-Content -Path C:\Users\elias\Desktop\files.csv -Value "C:\Temp\Docenten;Directory"
+Add-Content -Path C:\Users\elias\Desktop\files.csv -Value "C:\Temp\Studenten\Student1;File"
+Add-Content -Path C:\Users\elias\Desktop\files.csv -Value "C:\Temp\Docenten\Docent1;File"
+
+$files = Import-Csv .\files.csv -Delimiter ";"
+$files
+$files.Path
+$files | % { New-Item -Path $_.Path -Type $_.Type -Value ""}
+
+############################
+
+# 1. SPut the array below in the variable "$fruit" "meloen;2;stuk","appel;3;kg","banaan;4;kg","kokosnoot;5;stuk".
+$fruit = "meloen;2;stuk","appel;3;kg","banaan;4;kg","kokosnoot;5;stuk"
+
+# 1.1. Convert "$fruit" to a list of objects. These objects have the following property names
+# "naam","prijs","eenheid". Put this list of objects in the same variable "$fruit".
+$fruit = $fruit | ConvertFrom-Csv -Delimiter ";" -Header "naam","prijs","eenheid"
+
+# 1.2. Show next on screen: "$fruit" "$fruit.naam" "$fruit[0]""  "$fruit[3].prijs".
+$fruit
+$fruit.naam
+$fruit[0]
+$fruit[3].prijs
+
+# 1.3. Write "$fruit" to JSON file "fruit.json".
+$fruit | ConvertTo-Json | Out-File fruit.json
+# Test the file.
+Get-Content .\fruit.json
+
+# 2.1. #1.3. Write "$fruit" to JSON file "fruit.json".
+# HKCU:\Software\gdepaepe
+# HKLM:\Software\gdepaepe
+New-Item -Path C:\Users\elias\Desktop -Name keys.csv -ItemType File
+Add-Content -Path C:\Users\elias\Desktop\keys.csv -Value "Path"
+Add-Content -Path C:\Users\elias\Desktop\keys.csv -Value "HKCU:\Software\gdepaepe"
+Add-Content -Path C:\Users\elias\Desktop\keys.csv -Value "HKLM:\Software\gdepaepe"
+
+# 2.2. Create the keys in this CSV file in the registry using "binding by property name". Give the following as value "oef42".
+# To do this, first add a proper header line to "keys.csv".
+Import-Csv -Path C:\Users\elias\Desktop\keys.csv | New-Item -Value "oef42"
+
+# 2.3. View the contents of the registry.
+Get-ItemProperty -Path HKCU:\Software\gdepaepe
+Get-ItemProperty -Path HKLM:\Software\gdepaepe
+
+# 2.4. Now delete these keys using the same method (be careful: don't delete anything incorrectly!)
+Import-Csv -Path C:\Users\elias\Desktop\keys.csv | Remove-Item
+
+
+# 3.1. The following exercises use "cereal.csv" which contains the data of various grains.
+# Find the answer to the following questions by combining a number of Cmdlets via piping.
+$cereal = Import-Csv -Path C:\Users\elias\Desktop\cereal.csv -Delimiter ";"
+
+#3.2. Show all grains of type H.
+$cereal | Where-Object {$_.type -eq "H"}
+
+# 3.3. How many grains are there with calories equal to 50.
+$cereal | Where-Object {$_.calories -eq 50} | Measure-Object
+
+# 3.4. What is the type of the weight property after importing the CSV file.
+$cereal.weight | Get-Member
+
+# 3.5. Put the total weight of the grains with calories equal to 50 into a variable "$TotWeight".
+$sum = 0
+$cereal | Where-Object {$_.calories -eq 50} | ForEach-Object { $sum += [double] $_.weight } ; $sum
