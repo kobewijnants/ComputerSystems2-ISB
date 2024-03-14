e7b0830e7213a4ccd68ce98c6061eff5e6d60900
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-14 21:07:42 +0100

    Update user authentication logic

---
+++ Powershell/conf/Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,8 @@
+############################
+# @author Elias De Hondt   #
+# @see https://eliasdh.com #
+# @since 01/02/2024        #
+############################
+# Dir \WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+
+$env:Path += ";C:\Users\elias\OneDrive\Data Core\Documents\School Documents\(5) KdG\Toegepaste Informatica\(2) Toegepaste Informatica Jaar 2\(2) Computersystemen - ISB\ComputerSystems2-ISB\Powershell\Script"
