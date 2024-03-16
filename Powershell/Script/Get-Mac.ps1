4fd7d21730e544c2ada65b5eccab9cf447701635
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-16 11:59:53 +0100

    Fix PowerShell script and configuration files

--- Powershell/Script/Get-Mac.ps1
+++ Powershell/Script/Get-Mac.ps1
@@ -10,7 +10,7 @@ $filename = "C:\Users\elias\OneDrive\Data Core\Documents\School Documents\(5) Kd
 function Delete-LinesCsv($property, $value, $filename) {
     Import-Csv $filename | ? { $_.$property -ne $value } | Export-Csv ($filename + ".new") -force
     Move-Item ($filename + ".new") $filename -Force
-    }
+}
 
 function Write-Mac() {
     $adapters = Get-CimInstance Win32_NetworkAdapter | ? { $_.AdapterType -eq "Ethernet 802.3" -and $_.PhysicalAdapter -eq $true }
@@ -19,17 +19,17 @@ function Write-Mac() {
         Delete-LinesCsv mac $adapter.MacAddress $filename
         Write-Host ("Schrijf: " + $date + "," + $adapter.SystemName + "," + $adapter.Name + "," + $adapter.MacAddress)
         Add-Content $filename ($date + "," + $adapter.SystemName + "," + $adapter.Name + "," + $adapter.MacAddress)
-        }
     }
+}
 
 function Get-Mac($computername) {
     Import-Csv $filename | ? {$_.computername -like $computername}
-    }
+}
 
 function Clean-Mac([int]$maxtimespan) {
     Import-Csv $filename | ? { ((Get-Date) - (Get-Date $_.date)).days -le $maxtimespan } | Export-Csv ($filename + ".new")
     Move-Item ($filename + ".new") $filename -Force
-    }
+}
 
 if ( $args.count -eq 0 ) { Write-Mac }
 if ( $args[0] -eq "-show" -and $args.count -eq 2) { Get-Mac $args[1] }
--- Powershell/conf/Microsoft.PowerShell_profile.ps1
+++ Powershell/conf/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,6 @@
 # @see https://eliasdh.com #
 # @since 01/02/2024        #
 ############################
-# Dir \WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# \WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 $env:Path += ";C:\Users\elias\OneDrive\Data Core\Documents\School Documents\(5) KdG\Toegepaste Informatica\(2) Toegepaste Informatica Jaar 2\(2) Computersystemen - ISB\ComputerSystems2-ISB\Powershell\Script"
