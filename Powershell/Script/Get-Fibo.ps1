3074662815651c0685e5b41679690acaae628c71
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-19 09:53:32 +0100

    Refactor code to improve performance and readability

---
+++ Powershell/Script/Get-Fibo.ps1
@@ -0,0 +1,6 @@
+############################
+# @author Elias De Hondt   #
+# @see https://eliasdh.com #
+# @since 01/02/2024        #
+############################
+# Get-Fibo.ps1
---
+++ Powershell/Script/Watch-Dog.ps1
@@ -0,0 +1,6 @@
+############################
+# @author Elias De Hondt   #
+# @see https://eliasdh.com #
+# @since 01/02/2024        #
+############################
+# Watch-Dog.ps1
