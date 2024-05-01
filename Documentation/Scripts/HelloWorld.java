82d6e3d738ba86a23ec669a07dea966943550c46
EliasDeHondt <Elias.dehondt@outlook.com>
2024-05-01 12:34:09 +0200

    /

---
+++ Documentation/Scripts/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * @author Elias De Hondt
+ * @see https://eliasdh.com
+ * @since 25/04/2024
+ */
+
+// Package
+package Documentation.Scripts;
+
+// Class
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world, Elias De hondt.");
+    }
+}
