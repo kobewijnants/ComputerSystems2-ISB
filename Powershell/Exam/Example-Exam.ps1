76654dbad4e7f0fa5f2a569a70d4d5eaac79a598
EliasDeHondt <Elias.dehondt@outlook.com>
2024-06-01 19:57:28 +0200

    Refactor PowerShell script to import CSV file and retrieve unique street names

--- Powershell/Exam/Example-Exam.ps1
+++ Powershell/Exam/Example-Exam.ps1
@@ -18,16 +18,24 @@ Version: 1.0
 #>
 
 param ( # Parameters for the script
+    [string]$csvfile="../Data/Violations.csv",
     [bool]$RunNewWindow=$False,
-    [string]$RunInOtherDirectory="C:\",
     [bool]$RunAsAdministrator=$False,
     [bool]$RunInDebugMode=$False
 )
+
+try {
+    $Violations = Import-Csv -Path $csvfile -Delimiter ","
+}
+catch {
+    Exit-Script "Error: No CSV file" 1 $False
+}
 
 # Function to exit the script
 function Exit-Script([string]$Message="No Message", [int]$Code=0, [bool]$NoColor=$False) {
     # 1 = Error
     # 0 = Success
+    Clear-Host
     if ($NoColor) {
         if ($Code -eq 0) {
             Write-Host $Message
@@ -44,10 +52,38 @@ function Exit-Script([string]$Message="No Message", [int]$Code=0, [bool]$NoColor
     exit $Code
 }
 
+
+function Get-ListOfUniqueStreetNames($table) {
+    $UniqueStreetNames = $violations | Select-Object -Property opnameplaats_straat -Unique | Sort-Object -Property opnameplaats_straat
+
+
+    [array]$UniqueStreetNamesTable = @()
+    $i = 0
+    
+    foreach ($item in $UniqueStreetNames) { # Loop through the unique street names
+        $UniqueStreetNamesTable += [pscustomobject]@{ # Create a custom object
+            Id = [int]$i;
+            StreetName = [string]$item.opnameplaats_straat;
+        }
+        if ($i -eq 0) {
+            [array]$UniqueStreetNamesTable = @()
+        }
+        $i++
+    }
+    $UniqueStreetNamesTable | Format-Table
+}
+
+
+
 # Function to start the script
 function Main {
-    Exit-Script "Hallo" 1 $False
+    Get-ListOfUniqueStreetNames $csvfile
+    #Get-TotalAmountOfViolationsPerStreet
 }
+
+############################
+# ../Data/Violations.csv
+############################
 
 
 Main # Start the script
